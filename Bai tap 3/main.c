460e649ba02596fc7d6aca7009957e6a3fd32e5c
mxt629 <xuantien.06021990@gmail.com>
2022-03-11 17:41:03 +0700

    bai tap 3
    commit

--- Bai tap 3/main.c
+++ Bai tap 3/main.c
@@ -7,8 +7,28 @@
 
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void) {
+    int Toan;
+    int Ly;
+    int Hoa;
+    int tong;
+    int tbc;
+    
+    printf("Điểm Toán");
+    scanf("%d", &Toan);
+    
+    printf("Điểm Lý");
+    scanf("%d", &Ly);
+    
+    printf("Điểm Hóa");
+    scanf("%d", &Hoa);
+    
+    tong = Toan + Ly + Hoa;
+    tbc = (Toan + Ly + Hoa)/3;
+    
+    printf("Tong %d", tong);
+    printf("Trung Bnh %d", tbc);
+    
     return 0;
+    
 }
